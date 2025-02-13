5327490e4d01e3c73be8f4dd9c732dd3497084f9
Muhammad <muhammad.hussain@cardi-link.com>
2025-02-13 23:01:38 +0100

    Remove Node engine specification and update API base URL for production

--- server/client/src/utils/api.js
+++ server/client/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 
-const BASE_URL = 'https://mbilalhussain-folio-53af3d2e7767.herokuapp.com/api' || 'http://localhost:5000/api';
+// const BASE_URL = 'http://localhost:5000/api';
+
+const BASE_URL = 'https://mbilalhussain-folio-53af3d2e7767.herokuapp.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,  
