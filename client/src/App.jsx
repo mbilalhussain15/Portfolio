7eea4b09fb9419ba4664e49b4cf86379bb3a9620
Muhammad <muhammad.hussain@cardi-link.com>
2025-02-14 08:32:10 +0100

    Switch from HashRouter to BrowserRouter in App component and update script source in index.html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter  as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Layout from './pages/layout.jsx';
 import Home from './pages/Home.jsx'; // Your Home component path
