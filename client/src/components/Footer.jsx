eefee3bb9775aa3e8daeef9730cfd1f804466c5a
Muhammad <muhammad.hussain@cardi-link.com>
2025-02-17 21:39:58 +0100

    Update script asset path in index.html and change footer link to GitHub

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
                     <span>2025 All Rights Reserved.</span>
                 </div>
                 <div className="flex items-center">
-                    <a href='https://www.fiverr.com/codeworthy' target="_blank" rel="noreferrer">Made with ❤️ by <span className='font-bold'>Muhammad Bilal Hussain</span></a>
+                    <a href='https://github.com/mbilalhussain15' target="_blank" rel="noreferrer">Made with ❤️ by <span className='font-bold'>Muhammad Bilal Hussain</span></a>
                 </div>
                 <div className="hidden md:flex items-center">
                     <div className="mr-1 text-base">
